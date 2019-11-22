3126532b0d074f801a6f851248dbab3d75fc75f0
Souza <nicolas.souza@capgemini.com>
2019-11-22 09:20:47 -0300

    commit inicial dos arquivos do projeto

---
+++ src/test/java/com/souza/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souza.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
